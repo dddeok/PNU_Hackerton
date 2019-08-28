dac648aed70b004ee31a757872211be9b770e9f9
seongahpark <sksmsekekek21@naver.com>
2019-08-29 01:54:23 +0900

    뷰 및 오탈자 수정 완료

---
+++ app/src/main/java/com/example/mentos/ViewUi/MyText.java
@@ -0,0 +1,6 @@
+package com.example.mentos.ViewUi;
+
+import android.support.v4.app.Fragment;
+
+public class MyText extends Fragment {
+}
---
+++ app/src/main/java/com/example/mentos/ViewUi/Profile.java
@@ -0,0 +1,6 @@
+package com.example.mentos.ViewUi;
+
+import android.support.v7.app.AppCompatActivity;
+
+public class Profile extends AppCompatActivity {
+}
